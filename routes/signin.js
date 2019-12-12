a5e8a4b8285f8e71dcc3576abb9f86d6a3c8dad9
yuch256 <3022489608@qq.com>
2019-12-12 09:51:18 +0800

    email => usr

--- routes/signin.js
+++ routes/signin.js
@@ -12,24 +12,24 @@ router.get('/', (req, res) => {
 });
 
 router.get('/verify', async (req, res) => {
-  let { email, pwd } = url.parse(req.url, true).query;
+  let { usr, pwd } = url.parse(req.url, true).query;
   //console.log(JSON.stringify(url.parse(req.url, true).query))
 
   // 用户查询
   const User = require('../models/db_mysqluser');
-  const doc = await  User.searchuserHandler({user_id:email});
+  const doc = await User.searchuserHandler({ user_id: usr });
   //console.log(doc.result[0].user_id);
   console.log(doc);
   // 登录检测
-  if (Object.keys(doc.result).length!=0) {
-    const r = await decrypt(pwd,doc.result[0].salt);
+  if (Object.keys(doc.result).length != 0) {
+    const r = await decrypt(pwd, doc.result[0].salt);
     //console.log('r:' + r)
     if (r !== doc.result[0].password) render.err(res, '密码错误！')
     else {
-      const t = jwt.sign({ email, pwd: r }, secret, { expiresIn: 60*60*3 });
-      
+      const t = jwt.sign({ usr, pwd: r }, secret, { expiresIn: 60 * 60 * 3 });
+
       //console.log('token:' + t)
-      res.send({ result: 1, msg: '登录成功',admin:doc.result[0].user_id, token: t });
+      res.send({ result: 1, msg: '登录成功', admin: doc.result[0].user_id, token: t });
     }
   } else {
     res.send({ result: 0, msg: '用户不存在！' })
