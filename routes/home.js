6ff9294b83bfd000bdc25f4332e0ef399046eaf4
Liexss <924834690@qq.com>
2019-12-22 00:54:10 +0800

    confirm1

--- routes/home.js
+++ routes/home.js
@@ -5,11 +5,14 @@ const auth = require('../utils/auth');
 const render = require('../utils/render').HomeRouter;
 
 /* GET index page. */
-router.get('/', function (req, res) {
+router.get('/', async(req, res)=> {
+    const News = require('../models/db_mysqlnews');
+    let Collegenews=await News.selCollegenewsfourHandler();
     res.render('layout', {
         layout: 'home',
         title: '杭师大主页',
-        usr:"刘兴松"
+        usr:"刘兴松",
+        Collegenews:Collegenews
     });
 });
 
