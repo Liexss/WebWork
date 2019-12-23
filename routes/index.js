01ec7343fd59fe7a2bfd20e5c41942a21ec239ab
Liexss <924834690@qq.com>
2019-12-23 21:09:03 +0800

    1111

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,7 @@ const render = require("../utils/render").indexRender;
 
 /* GET index page. */
 router.get("/", function(req, res) {
-  res.render("layout", { layout: "index", usr: "???" });
-});
-
-router.get("/usr", auth, (req, res) => {
-  console.log(req.currentUser);
-  res.send({ usr: req.currentUser });
+  res.redirect('/signin');
 });
 
 module.exports = router;
